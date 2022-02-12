4bd747aa9a441d210ea8d60d821904188f9fe28a
"Mohammed Amin" <"coldprohor12@gmail.com">
2022-02-12 10:09:34 -0800

    added transfer to token

--- HagooCoin.sol
+++ HagooCoin.sol
@@ -1,20 +1,58 @@
-// SPDX-License-Idenifier: MIT
-pragma solidity ^0.8.10;
+//SPDX-License-Identifier: GPL
 
-contract SwirlyCoin {
-    // Field
-    uint public total = 0;
+pragma solidity >= 0.7.0 <0.9.0;
 
-    function viewCounts() public view returns (uint) {
-        return total;
-    }
-    // functions
-    function add(uint userInput) public {
-        total += userInput;
+contract HagooCoin {
+
+    string public constant name = "Hagoo Coin";
+    string public constant symbol = "HAG";
+    uint public constant decimal = 18;
+
+    // MAPPING { key:value }
+    // address: 100000 HAG
+    mapping(address => uint) balances;
+
+    // Supply
+    uint _totalSupply;
+
+    constructor(uint inputValue) {
+        _totalSupply = inputValue;
+        // msg.sender => your metamask
+        balances[msg.sender] = _totalSupply;
+
     }
 
-    function sub() public {
-        total -= 1;
+    // 1st required function
+    function totalSupply() public view returns(uint){
+        return _totalSupply;
     }
+    
+    // 2nd required function
+    function balanceOf(address owner) public view returns(uint) {
+        return balances[owner];
+    }
+
+    // transfer function
+    function transfer(address recipient, uint amount) public returns (bool){
+        if (balanceOf(msg.sender) >= amount) {
+            balances[recipient] += amount;
+            balances[msg.sender] -= amount;
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+        function transferFrom(address sender, address recipient, uint amount) public returns (bool){
+                if (balanceOf(sender) >= amount) {
+                    balances[recipient] += amount;
+                    balances[sender] -= amount;
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
 
 }
